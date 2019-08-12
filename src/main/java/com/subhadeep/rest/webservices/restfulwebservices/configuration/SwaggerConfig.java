a304d6ffb5cdf66037b0a803a1ff9ec2ba6814e6
SuBhadeep BiSwas <subhadeepbiswas250@gmail.com>
2019-08-13 02:39:00 +0530

    Enhancing Swagger Documentation with custom Annotations

--- src/main/java/com/subhadeep/rest/webservices/restfulwebservices/configuration/SwaggerConfig.java
+++ src/main/java/com/subhadeep/rest/webservices/restfulwebservices/configuration/SwaggerConfig.java
@@ -8,21 +8,29 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
-    public static final Contact DEFAULT_CONTACT = new Contact("Subhadeep Biswas", "https://github.com/subhadeep-bis", "subhadeepbiswas500@gmail.com");
-    public static final ApiInfo DEFAULT_API_INFO = new ApiInfo(
+    private static final Contact DEFAULT_CONTACT = new Contact("Subhadeep Biswas", "https://github.com/subhadeep-bis", "subhadeepbiswas500@gmail.com");
+    private static final ApiInfo DEFAULT_API_INFO = new ApiInfo(
             "Awesome API Title", "Awesome Api Documentation",
             "1.0", "urn:tos", DEFAULT_CONTACT,
             "Apache 2.0",
             "http://www.apache.org/licenses/LICENSE-2.0");
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES =
+            new HashSet<>(Arrays.asList("application/json", "application/xml"));
 
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(DEFAULT_API_INFO);
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
     }
 
 }
--- src/main/java/com/subhadeep/rest/webservices/restfulwebservices/model/User.java
+++ src/main/java/com/subhadeep/rest/webservices/restfulwebservices/model/User.java
@@ -1,15 +1,25 @@
 package com.subhadeep.rest.webservices.restfulwebservices.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+//@ApiModel and @ApiModelProperty are used for Swagger Documentation
+// These two annotations help us to add more sense to the model description
+// given in api documentation under "definition" section of Api Documentation
+
+@ApiModel(description = "Details about User model")
 public class User {
     private Integer id;
 
+    @ApiModelProperty(notes = "Minimum name length is 2 characters!")
     @Size(min = 2, message = "Minimum name length is 2 characters!")
     private String name;
 
+    @ApiModelProperty(notes = "Date of Birth can only be in Past")
     @Past
     private Date dob;
 
